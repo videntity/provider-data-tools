47aee1c3c246a65c56accd0e58452ea21cff8652
Alan Viars <alan.c.viars@gmail.com>
2014-08-09 15:42:19 -0400

    Bumped to version 0.0.7

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 
 setup(name="pdt",
-      version="0.0.6",
+      version="0.0.7",
       description="Provider Data Tools",
       long_description="""\
 Data processing utilities for providers.  Many tools are generic utilities.
