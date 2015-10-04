58a61cd08ce3f8f00f9cbdb0befada37de6093f6
Alan Viars <alan.c.viars@gmail.com>
2015-10-04 14:59:11 -0400

    Added a command line utility and library, vnpi, to verfify if an NPI is valid using Luhn.

---
+++ pdt/vnpi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+# Written by Alan Viars
+
+import sys, json
+from baluhn import verify
+
+LUHN_PREFIX ="80840"
+
+def verify_npi(number):
+    prefixed_number = "%s%s" % (LUHN_PREFIX, number)
+    return verify(prefixed_number)
+
+
+
+if __name__ == "__main__":
+    
+    
+    if len(sys.argv)!=2:
+        print "Usage:"
+        print "vnpi.py [NPI]"
+        print "Example:"
+        print "vnpi.py "
+        sys.exit(1)
+
+    #Get the file from the command line
+    result = verify_npi(sys.argv[1])
+    jresult = {"number": sys.argv[1], "valid": result}
+    print json.dumps(jresult, indent = 4 )
+    
