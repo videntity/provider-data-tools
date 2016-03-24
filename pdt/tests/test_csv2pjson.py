2a84770ce855782a418d5f9b69d9bc1c999caa6e
Alan Viars <aviars@videntity.com>
2016-03-24 12:18:04 -0400

    Initial csv2pjson test

---
+++ pdt/tests/test_csv2pjson.py
@@ -0,0 +1,22 @@
+import unittest, os
+from pdt.csv2pjson_public import publiccsv2pjson
+
+
+
+class csv2pjson(unittest.TestCase):
+    
+    def testRunCSV2PJSONT(self):                          
+        
+        """Run CSV2pjson should return results """
+        
+        csvfile = os.path.join( os.path.dirname( __file__),  "onehundredthousand.csv")        
+        output_dir = "test_output"
+        number_processed = 100000
+        
+        result = publiccsv2pjson(csvfile, output_dir)                 
+        self.assertEqual(result['num_files_created'], number_processed)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+
