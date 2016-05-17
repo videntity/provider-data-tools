06ee12dbdaf8917837347cca2449a1605b3fcaae
will <will.flwrs@gmail.com>
2016-05-17 11:51:58 -0400

    Change test to run schema against csv2fhir files created

--- tests/test_csv2fhir_public.py
+++ tests/test_csv2fhir_public.py
@@ -1,5 +1,6 @@
-import unittest, os
+import unittest, os, random
 from pdt import csv2fhir_public as fhir
+from pdt import json_schema_check
 
 
 class TestCSV2FHIR_PUBLIC(unittest.TestCase):
@@ -14,6 +15,19 @@ class TestCSV2FHIR_PUBLIC(unittest.TestCase):
         result = fhir.publiccsv2fhir(csvfile, output_dir)
         self.assertEqual(result['num_files_created'], number_processed)
         self.assertEqual(result['message'], "Completed without errors.")
+
+    def test_FHIR_results(self):
+
+        """Put newly created files through json_schema to check"""
+        output_dir = os.path.join( os.path.dirname( __file__),  "fhir_test_output")
+        json_schema = os.path.join( os.path.dirname( __file__),  "fhir_practitioner_schema.json")
+
+        for x in range(1000,2000):
+            if os.path.exists("output_dir/Practioner/x"):
+                random_file = random.choice(os.listdir("output_dir/x"))
+                result = json_schema_check.json_schema_check(json_schema, random_file)
+                print(result)
+                self.assertEqual(result['errors'], [])
 
 if __name__ == '__main__':
     unittest.main()
