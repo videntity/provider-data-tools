f57572780abfab785b55700ccbd8bf9d7ae4b39f
will <will.flwrs@gmail.com>
2016-07-06 00:05:56 -0400

    Make python2 & 3 compatible and pep8 compliant

--- pdt/loadnppes.py
+++ pdt/loadnppes.py
@@ -4,31 +4,41 @@
 # Written by Alan Viars - This software is public domain
 
 from bs4 import BeautifulSoup
-import urllib2
-import re, glob, sys
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+import re
+import glob
+import sys
 from subprocess import call
 from datetime import datetime
 
+
 def do_update(process_full=True, download=True):
-    months = ["Jan","Feb","Mar","Apr", "May", "June", "Sept", "Oct", "Nov", "Dec"]
-    #Get just the html page
+
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Sept", "Oct",
+              "Nov", "Dec"]
+    # Get just the html page
     html_page = urllib2.urlopen("http://download.cms.gov/nppes/NPI_Files.html")
     link_prefix = "http://download.cms.gov/nppes/"
-    
+
     soup = BeautifulSoup(html_page, "html.parser")
-    month =""
-    year= datetime.now().year
-    #get all links
+    month = ""
+    year = datetime.now().year
+    # get all links
     zipfilelinks = []
     for link in soup.findAll('a'):
-        print link
-        #get just zips
+        print(link)
+        # get just zips
         if link.get('href', "").endswith(".zip"):
             zipfilelinks.append(link.get('href', ""))
-    
-    #determine full v/s weekly
+
+    # determine full v/s weekly
     weeklylinks = []
-    full_link =""
+    full_link = ""
     for link in zipfilelinks:
 
         for m in months:
@@ -39,66 +49,66 @@ def do_update(process_full=True, download=True):
             deactivation_link = link
         else:
             weeklylinks.append(link)
-                    
-            
-    #Download full file
+
+    # Download full file
     if process_full:
-        
+
         if download:
-            print "Downloading", full_link        
+            print("Downloading", full_link)
             call(["wget", full_link])
-        
-        #Get filename and unzip
+
+        # Get filename and unzip
         filename = full_link.split("/")
         zipfilename = filename[-1]
-        print "Unzip", zipfilename
-       
-        call(["unzip",zipfilename])
-        
-        #inspect the local directory for the CSV
+        print("Unzip", zipfilename)
+
+        call(["unzip", zipfilename])
+
+        # inspect the local directory for the CSV
         csv_files = glob.glob("*.csv")
-        
+
         for f in csv_files:
             if f.__contains__("Header"):
                 header_file = f
             else:
                 main_file_to_import = f
-        
-        #Now import the file
-        print "Import", main_file_to_import
-        #first convert to json
+
+        # Now import the file
+        print("Import", main_file_to_import)
+        # first convert to json
         json_output_dir = "json-output"
-        call(["csv2pjson-public.py", main_file_to_import, json_output_dir ])
-        #now upload to mongo
-        call(["jsondir2mongo", json_output_dir, "nppes", "pjson", "T", "127.0.0.1", "27017" ])
-    
-    
-        #now create indexes
-        call(["create-provider-indexes", "nppes", "pjson", "127.0.0.1", "27017", "Y" ])
-    #Download weekly files
-        #for link in weeklylinks:
-        #    print "weekly", link        
+        call(["csv2pjson-public.py", main_file_to_import, json_output_dir])
+        # now upload to mongo
+        call(["jsondir2mongo", json_output_dir, "nppes", "pjson", "T",
+              "127.0.0.1", "27017"])
+
+        # now create indexes
+        call(["create-provider-indexes", "nppes", "pjson", "127.0.0.1",
+              "27017", "Y"])
+    # Download weekly files
+        # for link in weeklylinks:
+        #    print "weekly", link
 
 
 if __name__ == "__main__":
-    
-    
-    if len(sys.argv)!=3:
-        print "Usage:"
-        print "loadnppes.py [PROCESS_FULL Y/N] [DOWNLOAD_FROM_PUBLIC_FILE Y/N]"
-        print "Example:"
-        print "loadnppes.py Y Y"
+
+    if len(sys.argv) != 3:
+        print("Usage:")
+        print("loadnppes.py [PROCESS_FULL Y/N] "
+              "[DOWNLOAD_FROM_PUBLIC_FILE Y/N]")
+        print("Example:")
+        print("loadnppes.py Y Y")
         sys.exit(1)
 
     if sys.argv[1] in ("Y", "y", "T", True):
         process_full = True
     else:
         process_full = False
-    
+
     if sys.argv[2] in ("Y", "y", "T", True):
         download = True
     else:
         download = False
-    
-    #Get the file from the command line
+
+    # Get the file from the command line
     do_update(process_full, download)
