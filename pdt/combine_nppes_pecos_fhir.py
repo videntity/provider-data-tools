1436ce30ddd2ae26382d7903285f1a8f0a456b38
will <will.flwrs@gmail.com>
2016-07-21 11:09:45 -0400

    Fix update to doc and add new fields from PECOS to import

--- pdt/combine_nppes_pecos_fhir.py
+++ pdt/combine_nppes_pecos_fhir.py
@@ -19,7 +19,7 @@ MONGO_HOST = "127.0.0.1"
 MONGO_PORT = 27017
 
 
-def makepecos_fhir_docs(database_name="fhir"):
+def makepecos_fhirnppes_docs(database_name="fhir"):
 
     i = 0
     # try:
@@ -36,51 +36,83 @@ def makepecos_fhir_docs(database_name="fhir"):
         #Counter for display
         i += 1
         # Insert initial nppes document into new collection
-        fhir_practitioner.insert(bdoc)
+        fhir_practitioner.insert_one(bdoc)
+# ----------------------------------------------------
+# INCORPORATE ADDRESSES and other ID's
+# ----------------------------------------------------
+        match_bases = base_pecos.find({'NPI': bdoc['identifier'][0]['value']})
+        m_addresses = []
+        identifiers = []
+        for matches in match_bases:
+            match_addresses = addresses.find({'ENRLMT_ID': matches['ENRLMT_ID']})
+            for ma in match_addresses:
+                # Create fhir address from pecos addresses
+                address = OrderedDict()
+                address['use'] = 'work'
+                address['postalCode'] = ma['ZIP_CD']
+                address['city'] = ma['CITY_NAME']
+                address['country'] = 'USA'
+                address['state'] = ma['STATE_CD']
+                # I don't think that text is exactly meant for this purpose, but
+                # we're using it here for now.
+                address['text'] = 'PECOS data practice location'
+                # Append address
+                # fhir_practitioner.update_one(bdoc,
+                #                               {"$push": {"address": address}})
+                m_addresses.append(address)
+            # Other Identifiers: ENRLMT_ID, PECOS_ASCT_CNTL_ID
+            enrlmt_id = OrderedDict()
+            enrlmt_id['use'] = 'official'
+            enrlmt_id_cc = OrderedDict()
+            enrlmt_id_cc['text'] = 'Enrollment ID in PECOS basic data set'
+            enrlmt_id['type'] = enrlmt_id_cc
+            enrlmt_id['system'] = 'https://data.cms.gov/public-provider-enrollment'
+            enrlmt_id['value'] = matches['ENRLMT_ID']
+            identifiers.append(enrlmt_id)
+
+            # PECOS_ASCT_CNTL_ID
+            pecosAsctCntlId = OrderedDict()
+            pecosAsctCntlId['use'] = 'official'
+            pecosAsctCntlId_cc = OrderedDict()
+            pecosAsctCntlId_cc['text'] = 'PECOS_ASCT_CNTL_ID in PECOS basic data set'
+            pecosAsctCntlId['type'] = pecosAsctCntlId_cc
+            pecosAsctCntlId['system'] = 'https://data.cms.gov/public-provider-enrollment'
+            pecosAsctCntlId['value'] = matches['PECOS_ASCT_CNTL_ID']
+            identifiers.append(pecosAsctCntlId)
+# --------------------------------------------------------------
+# INCORPORATE AFFILIATIONS
+# --------------------------------------------------------------
         # Match up npi numbers between pecos and nppes
-        match = compiled_individuals_collection.find_one(
+        match_compiled_individuals = compiled_individuals_collection.find(
             {'NPI': bdoc['identifier'][0]['value']})
-        match_base = base_pecos.find_one({'NPI': bdoc['identifier'][0]['value']})
-        match_address = addresses.find_one({'ENRLMT_ID': match_base['ENRLMT_ID']})
-        # Create fhir address from pecos addresses
-        address = OrderedDict()
-        address['use'] = 'work'
-        address['postalCode'] = match_address['ZIP_CD']
-        address['city'] = match_address['CITY_NAME']
-        address['country'] = 'USA'
-        address['state'] = match_address['STATE_CD']
-        # I don't think that text is exactly meant for this purpose, but
-        # we're using it here for now.
-        address['text'] = 'PECOS data practice location'
-        # Append address
-        fhir_practitioner.update_one(bdoc,
-                                      {"$push": {"address": address}})
+        extensions = []
+        for mci in match_compiled_individuals:
+            # Create Codings
+            npi_coding = OrderedDict()
+            npi_coding['system'] = 'https://nppes.cms.hhs.gov/NPPES/Welcome.do'
+            npi_coding['code'] = mci['works_for'][0]['NPI']
+            npi_coding['display'] = 'NPI number of affiliation'
+            # Leaving off the 'userSelected' category for now.
 
-        # Create Codings
-        npi_coding = OrderedDict()
-        npi_coding['system'] = 'https://nppes.cms.hhs.gov/NPPES/Welcome.do'
-        npi_coding['code'] = match['works_for'][0]['NPI']
-        npi_coding['display'] = 'NPI number of affiliation'
-        # Leaving off the 'userSelected' category for now.
+            enrollmentid_coding = OrderedDict()
+            enrollmentid_coding['system'] = 'https://data.cms.gov/public-provider-enrollment'
+            enrollmentid_coding['code'] = mci['works_for'][0]['ENRLMT_ID']
+            enrollmentid_coding['display'] = 'PECOS Enrollment ID of affiliation'
 
-        enrollmentid_coding = OrderedDict()
-        enrollmentid_coding['system'] = 'https://data.cms.gov/public-provider-enrollment'
-        enrollmentid_coding['code'] = match['works_for'][0]['ENRLMT_ID']
-        enrollmentid_coding['display'] = 'PECOS Enrollment ID of affiliation'
+            # Create Codeable concept
+            value_codeable_concept = OrderedDict()
+            value_codeable_concept['coding'] = [npi_coding, enrollmentid_coding]
+            value_codeable_concept['text'] = mci['works_for'][0]['NAME'] + ', ' + mci['works_for'][0]['DESCRIPTION']
 
-        # Create Codeable concept
-        value_codeable_concept = OrderedDict()
-        value_codeable_concept['coding'] = [npi_coding, enrollmentid_coding]
-        value_codeable_concept['text'] = match['works_for'][0]['NAME'] + ',' + match['works_for'][0]['DESCRIPTION']
+            # Create fhir affiliation from compiled_individuals
+            affiliation = OrderedDict()
+            affiliation['url'] = 'https://data.cms.gov/public-provider-enrollment'
+            # print(value_codeable_concept['text'])
+            affiliation['valueCodeableConcept'] = value_codeable_concept
+            # wrap in list
+            extensions.append(affiliation)
 
-        # Create fhir affiliation from compiled_individuals
-        affiliation = OrderedDict()
-        affiliation['url'] = 'https://data.cms.gov/public-provider-enrollment'
-        # print(value_codeable_concept['text'])
-        affiliation['valueCodeableConcept'] = value_codeable_concept
-        # wrap in list
-        affiliation = [affiliation]
-        fhir_practitioner.update_one(bdoc, {"$push": {"extension": affiliation}})
+        fhir_practitioner.update_one(bdoc, {"$pushAll": {"extension": extensions, "address": m_addresses, "identifier": identifiers}}, upsert=True)
         # if d['resourceType'] == "Organization":
         #     compiled_organizations_collection.insert(d)
         # elif d['resourceType'] == "Practitioner":
@@ -100,8 +132,8 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 1:
         print("Usage:")
-        print("makepecos_fhir_docs.py")
+        print("makepecos_fhirnppes_docs.py")
         sys.exit(1)
 
     # Run it
-    makepecos_fhir_docs()
+    makepecos_fhirnppes_docs()
