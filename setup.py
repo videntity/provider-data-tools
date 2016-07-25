150ea266be0516b22ef2f308564b66e55029855c
will <will.flwrs@gmail.com>
2016-07-24 23:19:21 -0400

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name="pdt",
-      version="0.7.6",
+      version="0.7.7",
       description="Provider Data Tools",
       long_description="""A collection of scripts and APIs for working with"""
                        """health care provider data and beyond. Many tools"""
@@ -37,6 +37,16 @@ setup(name="pdt",
                'pdt/pjson/validate_pjson_dir',
                'pdt/json_schema_check.py',
                'pdt/json_schema_check_fhir.py',
-               'pdt/pull_pecos.py'
+               'pdt/pull_pecos.py',
+               'pdt/combine_nppes_pecos_fhir.py',
+               'pdt/create_pecos_addresses_indexes.py',
+               'pdt/create_pecos_base_indexes.py',
+               'pdt/create_pecos_compiled_individuals_indexes.py',
+               'pdt/create_pecos_compiled_organizations_indexes.py',
+               'pdt/create_pecos_reassignments_indexes.py',
+               'pdt/create_pract_nppes_fhir_indexes.py',
+               'pdt/create_pract_nppes_pecos_fhir_indexes.py',
+               'pdt/makepecosdocs.py',
+               'pdt/pull_new_files_ready.py'
                ]
       )
