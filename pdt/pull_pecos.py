4352ae3b837b8f8f45a3f365b2934c30a261fd12
will <will.flwrs@gmail.com>
2016-07-24 23:19:32 -0400

    Add basic exception handling

--- pdt/pull_pecos.py
+++ pdt/pull_pecos.py
@@ -16,45 +16,49 @@ from subprocess import call
 
 def pull_pecos(download_all=True, Base=False,
                Reassignment=False, Address=False):
+    try:
 
-    html_page = urlopen("https://data.cms.gov/public-provider-enrollment")
-    file_names = ['Base-Provider-Enrollment-File', 'Reassignment-Sub-File',
-                  'Address-Sub-File']
+        html_page = urlopen("https://data.cms.gov/public-provider-enrollment")
+        file_names = ['Base-Provider-Enrollment-File', 'Reassignment-Sub-File',
+                      'Address-Sub-File']
 
-    # Identify file links and parse into a list of strings
-    soup = BeautifulSoup(html_page, "html.parser")
-    file_links = []
-    for file_name in file_names:
-        for link in soup.findAll('a'):
-            if file_name in link.get('href', ""):
-                file_links.append(list(link.get('href', "").split('/')))
+        # Identify file links and parse into a list of strings
+        soup = BeautifulSoup(html_page, "html.parser")
+        file_links = []
+        for file_name in file_names:
+            for link in soup.findAll('a'):
+                if file_name in link.get('href', ""):
+                    file_links.append(list(link.get('href', "").split('/')))
 
-    csv_prefix = 'https://data.cms.gov/api/views/'
-    # Possible feature to add is allowing people to name the output file.
-    # Currently it supplants the downloaded file supplants a file that is
-    # already named the default value.
+        csv_prefix = 'https://data.cms.gov/api/views/'
+        # Possible feature to add is allowing people to name the output file.
+        # Currently it supplants the downloaded file supplants a file that is
+        # already named the default value.
 
-    # Download files according to user input
-    if download_all:
-        print("Downloading Base, Address and Reassignment CSV files")
-        for csv_link in file_links:
-            call(["wget", "--output-document=" + csv_link[-2] + ".csv",
-                  csv_prefix + csv_link[-1] + '/rows.csv?accessType=DOWNLOAD'])
+        # Download files according to user input
+        if download_all:
+            print("Downloading Base, Address and Reassignment CSV files")
+            for csv_link in file_links:
+                call(["wget", "--output-document=" + csv_link[-2] + ".csv",
+                      csv_prefix + csv_link[-1] + '/rows.csv?accessType=DOWNLOAD'])
 
-    if Base:
-        print("Downloading Base CSV file")
-        call(["wget", "--output-document=pecos_base.csv",
-              csv_prefix + file_links[0][-1] + '/rows.csv?accessType=DOWNLOAD'])
+        if Base:
+            print("Downloading Base CSV file")
+            call(["wget", "--output-document=pecos_base.csv",
+                  csv_prefix + file_links[0][-1] + '/rows.csv?accessType=DOWNLOAD'])
 
-    if Reassignment:
-        print("Downloading Reassignment CSV file")
-        call(["wget", "--output-document=pecos_reassignment.csv",
-              csv_prefix + file_links[1][-1] + "/rows.csv?accessType=DOWNLOAD"])
+        if Reassignment:
+            print("Downloading Reassignment CSV file")
+            call(["wget", "--output-document=pecos_reassignment.csv",
+                  csv_prefix + file_links[1][-1] + "/rows.csv?accessType=DOWNLOAD"])
 
-    if Address:
-        print("Downloading Address CSV file")
-        call(["wget", "--output-document=pecos_address.csv",
-              csv_prefix + file_links[2][-1] + "/rows.csv?accessType=DOWNLOAD"])
+        if Address:
+            print("Downloading Address CSV file")
+            call(["wget", "--output-document=pecos_address.csv",
+                  csv_prefix + file_links[2][-1] + "/rows.csv?accessType=DOWNLOAD"])
+
+    except:
+        print(sys.exc_info)
 
 
 if __name__ == '__main__':
