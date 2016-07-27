163fb7e882851fdd4c5b3c6a6fcb50469e992d41
will <will.flwrs@gmail.com>
2016-07-26 23:17:52 -0400

    Reduce __init__ file

--- pdt/__init__.py
+++ pdt/__init__.py
@@ -1,6 +1,6 @@
-from pdt.chop_nppes_public import *
-from pdt.csv2mlvs import *
-from pdt.create_provider_indexes import *
-from pdt.csv2pjson_public import *
-from pdt.csv2fhir_public import *
-from pdt.vnpi import *
+# from pdt.chop_nppes_public import *
+# from pdt.csv2mlvs import *
+# from pdt.create_provider_indexes import *
+# from pdt.csv2pjson_public import *
+# from pdt.csv2fhir_public import *
+# from pdt.vnpi import *
