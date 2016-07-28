d12264f53bc35a0933fdacf8c10d00a0267b6e9b
will <will.flwrs@gmail.com>
2016-07-28 13:24:16 -0400

    Combine comined indexes

--- pdt/create_combined_indexes.py
+++ pdt/create_combined_indexes.py
@@ -19,33 +19,23 @@ MONGO_HOST = "127.0.0.1"
 MONGO_PORT = 27017
 
 
-def ensure_provider_indexes(database_name, collection_name, host=MONGO_HOST,
+def ensure_provider_indexes(database_name, collection1, collection2, host=MONGO_HOST,
                             port=MONGO_PORT, background=True):
-    """Apply suiteable indexes to pract nppes/pecos fhir collection."""
+    """Apply suiteable indexes to combined nppes/pecos fhir collection."""
 
     response_dict = {}
+    mc = MongoClient(host=host, port=port)
+    db = mc[database_name]
     try:
 
-        mc = MongoClient(host=host, port=port)
-        db = mc[database_name]
-        collection = db[collection_name]
+
+        collection = db[collection1]
 
         collection.create_index([("identifier.value", ASCENDING)],
                                 background=background)
 
         collection.create_index([("telecom.value", ASCENDING)],
                                 background=background)
-
-        # collection.create_index([("address.postalCode",     ASCENDING),
-        #                          ("enumeration_type", ASCENDING),
-        #                          ("addresses.city",   ASCENDING),
-        #                          ("addresses.state",  ASCENDING),
-        #                          ("basic.first_name", ASCENDING),
-        #                          ("basic.last_name",  ASCENDING),
-        #                          ("basic.organization_name", ASCENDING),
-        #                          ("basic.doing_business_as", ASCENDING)
-        #                          ], name="biggie",
-        #                         background=background)
 
         collection.create_index([("address.postalCode", ASCENDING)],
                                 background=background)
@@ -69,12 +59,58 @@ def ensure_provider_indexes(database_name, collection_name, host=MONGO_HOST,
                                   ASCENDING)],
                                 background=background)
 
-
+        response_dict['collection1'] = collection1
         response_dict['created_indexes'] = True
         response_dict['background'] = background
     except:
-        response_dict = {}
+        response_dict['collection1'] = collection1
+        response_dict['code'] = 500
+        response_dict['errors'] = [str(sys.exc_info()), ]
+        response_dict['message'] = str(sys.exc_info())
 
+    # Combined Organizations
+
+    try:
+
+        collection = db[collection2]
+
+        collection.create_index([("identifier.value", ASCENDING)],
+                                background=background)
+
+        collection.create_index([("telecom.value", ASCENDING)],
+                                background=background)
+        collection.create_index([("contact.telecom.value", ASCENDING)],
+                                background=background)
+
+        collection.create_index([("address.postalCode", ASCENDING)],
+                                background=background)
+        collection.create_index([("address.state", ASCENDING)],
+                                background=background)
+        collection.create_index([("address.city", ASCENDING)],
+                                background=background)
+        collection.create_index([("address.line", ASCENDING)],
+                                background=background)
+        collection.create_index([("name", ASCENDING)],
+                                background=background)
+        collection.create_index([("contact.name.prefix", ASCENDING)],
+                                background=background)
+        collection.create_index([("contact.name.given", ASCENDING)],
+                                background=background)
+        collection.create_index([("contact.name.family", ASCENDING)],
+                                background=background)
+
+        collection.create_index([("contact.name.suffix", ASCENDING)],
+                                background=background)
+
+        collection.create_index([("extension.valueCodeableConcept.coding.code",
+                                  ASCENDING)],
+                                background=background)
+
+        response_dict['collection2'] = collection2
+        response_dict['created_indexes'] = True
+        response_dict['background'] = background
+    except:
+        response_dict['collection2'] = collection2
         response_dict['code'] = 500
         response_dict['errors'] = [str(sys.exc_info()), ]
         response_dict['message'] = str(sys.exc_info())
@@ -86,19 +122,20 @@ if __name__ == "__main__":
     if len(sys.argv) != 6:
         print("Usage:")
         print(
-            "create_pract_nppes_pecos_fhir_indexes.py [DATABASE] [COLLECTION] [HOST] [PORT] [BACKGROUND Y/N]")
+            "create_combined_indexes.py [DATABASE] [COMBINED PRACTITIONER COLLECTION NAME] [COMBINED ORGANIZATION COLLECTION NAME] [HOST] [PORT] [BACKGROUND Y/N]")
         sys.exit(1)
 
     database = sys.argv[1]
-    collection = sys.argv[2]
-    host = sys.argv[3]
-    port = int(sys.argv[4])
+    collection1 = sys.argv[2]
+    collection2 = sys.argv[3]
+    host = sys.argv[4]
+    port = int(sys.argv[5])
 
-    if sys.argv[5].lower() in ("y", "yes", "t", "true"):
+    if sys.argv[6].lower() in ("y", "yes", "t", "true"):
         background = True
     else:
         background = False
 
     result = ensure_provider_indexes(
-        database, collection, host, port, background)
+        database, collection1, collection2, host, port, background)
     print(json.dumps(result, indent=4))
