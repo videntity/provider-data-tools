57c34d5b0b41ffb04401e057f5b23a9ff4423d6a
Alan Viars <aviars@videntity.com>
2016-10-31 13:25:03 -0400

    Updates to PECOS doc maker

--- pdt/makepecosdocs.py
+++ pdt/makepecosdocs.py
@@ -19,7 +19,7 @@ MONGO_HOST = "127.0.0.1"
 MONGO_PORT = 27017
 
 
-def makepecosdb(database_name="pecos"):
+def makepecosdb(database_name="pecos", collection_name="compiled"):
 
     i = 0
     try:
@@ -35,78 +35,133 @@ def makepecosdb(database_name="pecos"):
         base_collection = db['base']
         addresses_collection = db['addresses']
         reassignments_collection = db['reassignments']
+        specialties_collection = db['specialties']
         compiled_individuals_collection = db['compiled_individuals']
         compiled_organizations_collection = db['compiled_organizations']
+        compiled_collection = db['compiled']
 
         for bdoc in base_collection.find():
             i += 1
             d = OrderedDict()
+
+            d['pecos_id'] = bdoc['PECOS_ASCT_CNTL_ID']
+            d['enrollment_id'] = bdoc['ENRLMT_ID']
+            d['npi'] = bdoc['NPI']
+            d['tin'] = "SCRUBBED"
+            d['tin_type'] = ""
             if bdoc['ENRLMT_ID'].startswith('O'):
-                d['resourceType'] = "Organization"
+                d['enrollment_type'] = "O"
+                d['entity_type'] = "2"
             elif bdoc['ENRLMT_ID'].startswith('I'):
-                d['resourceType'] = "Practitioner"
+                d['enrollment_type'] = "I"
+                d['entity_type'] = "1"
 
+            d['first_name'] = bdoc['FIRST_NAME']
+            d['last_name'] = bdoc['LAST_NAME']
+            d['organization_name'] = bdoc['ORG_NAME']
+        
             if bdoc['FIRST_NAME']:
-
-                d['NAME'] = "%s %s" % (bdoc['FIRST_NAME'], bdoc['LAST_NAME'])
+                d['name'] = "%s %s" % (bdoc['FIRST_NAME'], bdoc['LAST_NAME'])
             else:
-                d['NAME'] = bdoc['ORG_NAME']
+                d['name'] = bdoc['ORG_NAME']
 
-            d['DESCRIPTION'] = bdoc['PROVIDER_TYPE_DESC']
-            d["NPI"] = bdoc['NPI']
-            d["works_for"] = []
-            d["has_providers"] = []
-            for rdoc in reassignments_collection.find({"REASGN_BNFT_ENRLMT_ID": bdoc['ENRLMT_ID']}):
 
-                ac = base_collection.find(
-                    {'ENRLMT_ID': rdoc["RCV_BNFT_ENRLMT_ID"]})
-                for a in ac:
-                    affiliate = OrderedDict()
 
-                    if a['FIRST_NAME']:
-                        affiliate['NAME'] = "%s %s" % (
-                            a['FIRST_NAME'], a['LAST_NAME'])
-                    else:
-                        affiliate['NAME'] = a['ORG_NAME']
+            d['first_approved_date'] = "1980-01-01"
+            d['last_approved_date'] = "1980-01-01"
 
-                    affiliate['NPI'] = a['NPI']
-                    affiliate['ENRLMT_ID'] = a['ENRLMT_ID']
-                    affiliate['DESCRIPTION'] = a['PROVIDER_TYPE_DESC']
-                    d["works_for"].append(affiliate)
+            #d['DESCRIPTION'] = bdoc['PROVIDER_TYPE_DESC']
+            
+            d['reassignments'] = []
+            d['specialties'] = []
+            d['addresses'] = []
+            
+            
+            if d['entity_type'] == "1":
+                
+            
+                for rdoc in reassignments_collection.find({"REASGN_BNFT_ENRLMT_ID": bdoc['ENRLMT_ID']}):
 
-            for rdoc in reassignments_collection.find({"RCV_BNFT_ENRLMT_ID": bdoc['ENRLMT_ID']}):
+                    ac = base_collection.find({'ENRLMT_ID': rdoc["RCV_BNFT_ENRLMT_ID"]})
+                    
+                    for a in ac:
+                        reassigned_to = OrderedDict()
+    
+                        if a['FIRST_NAME']:
+                            reassigned_to['name'] = "%s %s" % ( a['FIRST_NAME'], a['LAST_NAME'])
+                        else:
+                            reassigned_to['name'] = a['ORG_NAME']
+                        reassigned_to['pecos_id'] = a['PECOS_ASCT_CNTL_ID']
+                        reassigned_to['npi'] = a['NPI']
+                        reassigned_to['enrollment_id'] = a['ENRLMT_ID']
+                        reassigned_to['tin'] = "SCRUBBED"
+                        reassigned_to['tin_type'] = ""
+                                                
+                        if str(a['ENRLMT_ID']).startswith("O"):
+                            
+                            reassigned_to['entity_type'] = "2"
+                        else:
+                            reassigned_to['entity_type'] = "1"
+                        
+                        d["reassignments"].append({"reassigned_to":reassigned_to, "effective_date": "1980-01-01"})
 
-                ac = base_collection.find(
-                    {'ENRLMT_ID': rdoc["REASGN_BNFT_ENRLMT_ID"]})
-                for a in ac:
-                    affiliate = OrderedDict()
-                    if a['FIRST_NAME']:
-                        affiliate['NAME'] = "%s %s" % (
-                            a['FIRST_NAME'], a['LAST_NAME'])
-                    else:
-                        affiliate['NAME'] = a['ORG_NAME']
+            elif d['entity_type'] == "2":
+                for rdoc in reassignments_collection.find({"RCV_BNFT_ENRLMT_ID": bdoc['ENRLMT_ID']}):
+            
+                    ac = base_collection.find(
+                        {'ENRLMT_ID': rdoc["REASGN_BNFT_ENRLMT_ID"]})
+                    for a in ac:
+                        assignee =OrderedDict()
+                        if a['FIRST_NAME']:
+                            assignee['name'] = "%s %s" % (
+                                a['FIRST_NAME'], a['LAST_NAME'])
+                        else:
+                            assignee['name'] = a['ORG_NAME']
+                            
+                        assignee['pecos_id'] = a['PECOS_ASCT_CNTL_ID']
+                        assignee['npi'] = a['npi']
+                        assignee['enrollment_id'] = a['ENRLMT_ID']
+                        assignee['tin'] = "SCRUBBED"
+                        assignee['tin_type'] = ""
+                        if str(a['ENRLMT_ID']).startswith("O"):
+                            
+                            assignee['entity_type'] = "2"
+                        else:
+                            assignee['entity_type'] = "1"
+        
+                        d["reassignments"].append({"assignee": assignee, "effective_date": "1980-01-01"})
+                       
 
-                    affiliate['NPI'] = a['NPI']
-                    affiliate['ENRLMT_ID'] = a['ENRLMT_ID']
-                    affiliate['DESCRIPTION'] = a['PROVIDER_TYPE_DESC']
-                    d["has_providers"].append(affiliate)
+            # Get addresses
+            for a in addresses_collection.find({"ENRLMT_ID": bdoc['ENRLMT_ID']}):
+                address = OrderedDict()
+                address['address_type'] = "P"
+                address['line_1'] = "123 Fake Street"
+                address['line_2'] = ""
+                address['city'] = a["CITY_NAME"]
+                address['state'] = a["ZIP_CD"]
+                address['zip_code'] =  a["STATE_CD"]
+                d["addresses"].append(address)
 
-            if not d["has_providers"]:
-                del d["has_providers"]
-            if not d["works_for"]:
-                del d["works_for"]
+            # Get specialties
+            for s in specialties_collection.find({"ENRLMT_ID": bdoc['ENRLMT_ID']}):
+                specialty = OrderedDict()
+                specialty['code'] = s['PROVIDER_TYPE_CD'][3:]
+                specialty['decription'] = "PROVIDER_TYPE_DESC"
+                d["specialties"].append(specialty)
+            
+            
+            if i % 1000 == 0:
+                print(i)
+            #print json.dumps(d, indent =4)
+            compiled_collection.insert(d)
 
-            if d['resourceType'] == "Organization":
-                compiled_organizations_collection.insert(d)
-            elif d['resourceType'] == "Practitioner":
-                compiled_individuals_collection.insert(d)
-
-            # print json.dumps(d, indent =4)
+            
 
         # Walk through base
 
     except:
-        print(sys.exc_info)
+        print(sys.exc_info())
 
     print(i, "Processed")
 
